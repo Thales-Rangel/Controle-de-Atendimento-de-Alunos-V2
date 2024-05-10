eec9223d33cccfbe8cc6ea864b73b54e9d7e46d8
Thales Rangel <thalesran2006@gmail.com>
2024-05-09 23:41:32 -0300

    Inicio da Interface do professor
    Primeira construção do interface do professor.

--- Projeto/src/View/Professor/ProfessorView.java
+++ Projeto/src/View/Professor/ProfessorView.java
@@ -1,15 +1,28 @@
 package View.Professor;
 
 import java.awt.EventQueue;
+import java.awt.Toolkit;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import Models.Professor;
+import View.Admin.Admin;
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+
 public class ProfessorView extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	
+	private Professor prof;
+	private JLabel lblNome;
 
 	/**
 	 * Launch the application.
@@ -18,7 +31,7 @@ public class ProfessorView extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					ProfessorView frame = new ProfessorView();
+					ProfessorView frame = new ProfessorView(null);
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -30,13 +43,47 @@ public class ProfessorView extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public ProfessorView() {
+	public ProfessorView(Professor prof) {
+		this.prof = prof;
+		
+		setIconImage(Toolkit.getDefaultToolkit().getImage(Admin.class.getResource("/img/IF Logo - Remove.png")));
+		setTitle("Página do Professor");
+		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 860, 630);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		
+		setLocationRelativeTo(null);
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(26, 150, 3));
+		panel.setBounds(636, 0, 210, 593);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblIFLogo = new JLabel("");
+		lblIFLogo.setIcon(new ImageIcon(ProfessorView.class.getResource("/img/IF Logo Branca - Remove.png")));
+		lblIFLogo.setBounds(32, 321, 145, 142);
+		panel.add(lblIFLogo);
+		
+		lblNome = new JLabel(prof.getNome());
+		lblNome.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNome.setFont(new Font("Arial", Font.PLAIN, 20));
+		lblNome.setBounds(10, 10, 190, 42);
+		panel.add(lblNome);
+		
+		JButton btnVerDados = new JButton("Ver meus dados");
+		btnVerDados.setFont(new Font("Arial", Font.PLAIN, 15));
+		btnVerDados.setBounds(32, 62, 145, 27);
+		panel.add(btnVerDados);
+		
+		JButton btnVerSolicitations = new JButton("Ver solicitações");
+		btnVerSolicitations.setFont(new Font("Arial", Font.PLAIN, 15));
+		btnVerSolicitations.setBounds(32, 99, 145, 27);
+		panel.add(btnVerSolicitations);
 	}
-
 }
